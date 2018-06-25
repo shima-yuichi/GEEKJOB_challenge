703a3f8f85d214d306fb9a0d464f4bca2723c744
shima-yuichi <shiki.syukatu@gmail.com>
2018-06-25 11:31:28 +0900

    java課題、メソッドの「引数、戻り値1」を提出させていただきます。

---
+++ CampChallenge_java/011.メソッド/method6.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class method6 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    String[] user_profile(int kan){
+        String[] prof1 = {"1","技育太郎","東京","男","プログラマー"};
+        String[] prof2 = {"2","技育花子","北海道","女","システムエンジニア"};
+        String[] prof3 = {"3","技育三郎","大阪","男","Webエンジニア"};
+        
+        String kan2 = String.valueOf(kan);
+        
+        if(prof1[0].equals(kan2)){
+            return prof1;
+        }
+        else if(prof2[0].equals(kan2)){
+            return prof2;
+        }
+        else if(prof3[0].equals(kan2)){
+            return prof3;
+        }
+        else{
+            return null;
+        }
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String[] put = user_profile(3);
+            
+            
+            for(String data: put){
+                out.print(data+"<br>");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
